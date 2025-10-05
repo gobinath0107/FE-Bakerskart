ba4cbaeb485af4b3286c03c584dd0f8c6319eea7
gobinath0107 <mgobinath.shanthi@gmail.com>
2025-10-05 13:38:17 +0530

    production url typo fixed

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const productionUrl = ' https://strapi-store-server.onrender.com/api';
-const productionUrl = import.meta.env.MODE === "developemnt" ?  'http://localhost:5000/api/v1' : 'https://be-bakerskart-production.up.railway.app/api/v1';
+const productionUrl = import.meta.env.MODE === "development" ?  'http://localhost:5000/api/v1' : 'https://be-bakerskart-production.up.railway.app/api/v1';
 console.log("import.meta.env.MODE",import.meta.env.MODE);
 
 
