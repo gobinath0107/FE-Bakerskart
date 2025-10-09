c8f4e3b83db79450a1e151986a543ee999a4891d
gobinath0107 <mgobinath.shanthi@gmail.com>
2025-10-09 18:53:55 +0530

    Issue in product list page

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -7,14 +7,16 @@ const ProductsList = () => {
   return (
     <div className='mt-12 grid gap-y-8'>
       {products.map((product) => {
+        console.log("products", product);
+        
         const { title, price, image1, company, sellingPrice } = product;
         const { url: image } = image1;
         const rupeeAmount = formatPrice(price);
         const rupeeSellingPrice = formatPrice(sellingPrice);
         return (
           <Link
-            key={product.id}
-            to={`/products/${product.id}`}
+            key={product._id}
+            to={`/products/${product._id}`}
             className='p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap  bg-base-100 shadow-xl hover:shadow-2xl duration-300 group'
           >
             <img
